9ad937a2ae35aa071beb0022c0737470cac835c7
Vadzim Dzeravianka <vadim.by@gmail.com>
2017-02-14 01:35:54 +0300

    initial commit

---
+++ src/main/java/by/vadim/test/SpringSecureTestApplication.java
@@ -0,0 +1,12 @@
+package by.vadim.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecureTestApplication.class, args);
+	}
+}
